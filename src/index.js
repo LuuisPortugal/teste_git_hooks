9dad089be8f23c9407e5fe483a0f0f2d2879a04c
Luis Portugal <luisenriquegomesportugal@gmail.com>
2019-11-11 19:52:58 -0300

    change to json

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Jesus te ama"));
+app.get("/", (req, res) =>
+  res.json({
+    message: "Jesus te ama"
+  })
+);
 
 app.listen(80);
