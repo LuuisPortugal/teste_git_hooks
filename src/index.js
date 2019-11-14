463965f369106f00b6994eb0dacf558a938921f3
unknown <17300211@acad.cesupa>
2019-11-14 07:19:01 -0300

    Change message

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Jesus te ama"));
+app.get("/", (req, res) => res.send("Jesus te ama e pronto"));
 
 app.listen(80);
